907e5d8a445f0825ff099bef1e663442f76f6056
Company_work <ssolsu1@126.com>
2018-01-30 17:45:02 +0800

    1

--- New_Index.py
+++ New_Index.py
@@ -36,9 +36,9 @@ class Main_win(QWidget):
         self.req_two.cookies.update(req_one.cookies)
         self.url_three = 'http://www.pceggs.com/nologin.aspx'
         self.req_th = requests.get(self.url_three, headers=self.head_1, cookies=self.req_two.cookies)
-        soup = BeautifulSoup(self.req_th.text, 'lxml')
-        self.__VIEWSTATE = soup.find('input', attrs={'id': '__VIEWSTATE'}).get('value')
-        self.__VIEWSTATEGENERATOR = soup.find('input', attrs={'id': '__VIEWSTATEGENERATOR'}).get('value')
+        self.soup = BeautifulSoup(self.req_th.text, 'lxml')
+        self.__VIEWSTATE = self.soup.find('input', attrs={'id': '__VIEWSTATE'}).get('value')
+        self.__VIEWSTATEGENERATOR = self.soup.find('input', attrs={'id': '__VIEWSTATEGENERATOR'}).get('value')
         self.setLayout(self.layout)
         self.photo = QPixmap()
         self.photo.loadFromData(req_one.content)
@@ -49,6 +49,36 @@ class Main_win(QWidget):
         self.layout.addWidget(self.label)
         self.layout.addWidget(self.sub_button)
 
+    def pc28(self):
+        for n in range(13, 15):
+            print("现在开始搜索第%d页" % n)
+            url_pst = {'__VIEWSTATE': self.__VIEWSTATE,
+                       '__VIEWSTATEGENERATOR': self.__VIEWSTATEGENERATOR,
+                       'CurrentPageIndex': n,
+                       'reloadshow': 11,
+                       'soundshow': 10
+                       }
+            # print(self.__VIEWSTATE, self.__VIEWSTATEGENERATOR)
+            url = "http://www.pceggs.com/play/pxya.aspx"
+            url_req = requests.post(url, data=url_pst, cookies=self.req.cookies, headers=self.head_1)
+            # print(url_req.text)
+            self.soup = BeautifulSoup(url_req.text, 'lxml')
+            tr_text = self.soup.find_all('tr', attrs={'align': 'center', 'bgcolor': '#FFFFFF'}, limit=20)
+            # print(tr_text)
+            for str in tr_text:
+                try:
+                    list_text = str.contents
+                    betting_period = list_text[1].string
+                    betting_time = list_text[3].string
+                    betting_status = (list_text[15].string).split("'")[1]
+                    betting_result = (list_text[5].string).split("'")[3]
+                except (Exception)  as e:
+                    print(repr(e))
+                else:
+                    print("投注期:" + betting_period, "投注时间:" + betting_time, "开奖结果:" + betting_result,
+                          "开奖状态:" + betting_status)
+            time.sleep(2)
+
     def submit_site(self):
         pst_data = {'txt_UserName': self.name.text(), 'txt_PWD': self.password.text(), '__VIEWSTATE': self.__VIEWSTATE,
                     '__VIEWSTATEGENERATOR': self.__VIEWSTATEGENERATOR, 'txt_VerifyCode': self.vali.text(),
@@ -58,12 +88,10 @@ class Main_win(QWidget):
                     }
         url = 'http://www.pceggs.com/nologin.aspx'
         self.req = requests.post(url, data=pst_data, cookies=self.req_two.cookies, headers=self.head_1,
-                            allow_redirects=False)
-        print(self.name.text(), self.password.text(), self.__VIEWSTATE, self.__VIEWSTATEGENERATOR, self.vali.text())
-        print(self.req.cookies)
-
-    def pc28(self):
-        pass
+                                 allow_redirects=False)
+        self.req.cookies.update(self.req_two.cookies)
+        time.sleep(1)
+        self.pc28()
 
 
 if __name__ == "__main__":
