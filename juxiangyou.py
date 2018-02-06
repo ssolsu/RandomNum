7eae7f2d7b940129ccc32025505d3245f8ff42b8
Administrator <ssolsu1@126.com>
2018-02-06 23:13:12 +0800

    432123355

--- juxiangyou.py
+++ juxiangyou.py
@@ -2,21 +2,24 @@ import sys, time, os
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import (QWidget, QLabel, QVBoxLayout, QLineEdit, QPushButton)
 from PyQt5.QtGui import QPixmap
-import requests, json, threading
+import requests, json
+from PyQt5.QtCore import QThread
 from bs4 import BeautifulSoup
 
 
-class Main_win(QWidget, threading.Thread):
+class Main_win(QWidget):
     def __init__(self):
         super(Main_win, self).__init__()
-        # self.initUI()
+        self.initUI()
+        self.t1 = Thread()
         # self.do_16()
 
     def initUI(self):
+
         self.setWindowTitle('juxiangyou Window')
         self.setGeometry(100, 100, 200, 150)
-        self.name = QLineEdit()
-        self.password = QLineEdit()
+        self.name = QLineEdit('ssolsu1@126.com')
+        self.password = QLineEdit('xfcctv1983')
         self.vali = QLineEdit()
         self.sub_button = QPushButton()
         self.sub_button.clicked.connect(self.submit_site)
@@ -43,7 +46,7 @@ class Main_win(QWidget, threading.Thread):
         print(req_2.cookies.values())
         req_2.cookies.update(req_1.cookies)
         self.reqcookies = req_2.cookies
-        print(self.reqcookies)
+        # print(self.reqcookies)
         # PyQt5 加载图片数据，访问验证码页面放回的byte格式数据
         self.photo.loadFromData(req_2.content)
         self.label.setPixmap(self.photo)
@@ -59,6 +62,7 @@ class Main_win(QWidget, threading.Thread):
             self.baseN(num // b, b).lstrip("0") + "0123456789abcdefghijklmnopqrstuvwxyz"[num % b])
 
     def submit_site(self):
+        global gol_cookies
         flag = False
         base_time = int(time.time()) * 1000
         x_sign = self.baseN(base_time, 36)
@@ -90,9 +94,10 @@ class Main_win(QWidget, threading.Thread):
             if req.text.find('10000') > 0:
                 print('登录成功，等待3秒，开始循环查询网页')
                 # 登录成功，设定全局访问cookies
-                self.gol_cookies = req.cookies
+                gol_cookies = req.cookies
                 flag = True
-                time.sleep(3)
+                # time.sleep(3)
+                self.t1.start()
             elif req.text.find('10003') > 0:
                 print('验证码或者密码错误')
             elif req.text.find('10005') > 0:
@@ -103,56 +108,71 @@ class Main_win(QWidget, threading.Thread):
             print(repr(e))
         return flag
 
-    def do_16(self, name):
-        while True:
-            url = 'http://www.juxiangyou.com/fun/play/speed16/index'
-            # req = requests.get(url, cookies=self.gol_cookies, headers=self.header)
-            soup = BeautifulSoup(open('2.html'), 'lxml')
-            tr_text = soup.find_all('tr', limit=24)
-            print(len(tr_text))
-            # 查询当前投注信息
-            vote_info = soup.find('label', attrs={'class': 'J_jcEnd'})
-            # 判断是否刚好在开奖
-            if (vote_info.text).find('正在开奖') > 0:
-                print('正在开奖，等待5秒')
-                time.sleep(5)
-            else:
-                # 如果没有开奖，则查询当前投注期
-                try:
-                    vote_current = vote_info.find_all('span')
-                    # 结束标识，查询
-                    end_flag = (vote_info.text).find('截止投注')
-                    if end_flag > 0:
-                        print(vote_current[0].string + '期已经截止投注')
-                    else:
-                        print('当前期' + vote_current[0].string + '剩余' + vote_current[1].string + '秒投注')
-                        self.vote_retime = vote_current[1].string
-                except Exception as e:
-                    print('搜索资料出错，列表错误')
-            # print(tr_text)
-            for strx in tr_text[1:7]:
-                try:
-                    list_text = strx.contents
-                    # print(list_text)
-                    vote_peroid = list_text[1].string
-                    vote_time = list_text[3].string
-                    vote_result = list_text[5].find('span').text
-                    if vote_result != '':
-                        print('开奖期数:' + vote_peroid, '开奖时间：' + vote_time, '开奖结果：' + vote_result)
-                except Exception as e:
-                    print('搜索数据错误，列表出错')
-            dealy_time = int(self.vote_retime) + 30
-            print('延时%s刷新' % dealy_time)
-            time.sleep(dealy_time)
 
-    def por(self):
-        th1 = threading.Thread(target=self.do_16, args=(self,))
-        th1.start()
+# def do_16():
+#     n = 0
+#     while True:
+#         n = n + 1
+#         print(n)
+
+
+def do_16():
+    while True:
+        vote_retime = 0
+        url = 'http://www.juxiangyou.com/fun/play/speed16/index'
+        req = requests.get(url, cookies=gol_cookies, headers=Main_win().header)
+        soup = BeautifulSoup(req.text, 'lxml')
+        tr_text = soup.find_all('tr', limit=24)
+        print(len(tr_text))
+        # 查询当前投注信息
+        vote_info = soup.find('label', attrs={'class': 'J_jcEnd'})
+        # 判断是否刚好在开奖
+        if (vote_info.text).find('正在开奖') > 0:
+            print('正在开奖，等待5秒')
+            time.sleep(5)
+        else:
+            # 如果没有开奖，则查询当前投注期
+            try:
+                vote_current = vote_info.find_all('span')
+                # 结束标识，查询
+                end_flag = (vote_info.text).find('截止投注')
+                if end_flag > 0:
+                    print(vote_current[0].string + '期已经截止投注')
+                else:
+                    print('当前期' + vote_current[0].string + '剩余' + vote_current[1].string + '秒投注')
+                    vote_retime = int(vote_current[1].string)
+            except Exception as e:
+                print('搜索资料出错，列表错误')
+        # print(tr_text)
+        for strx in tr_text[1:7]:
+            try:
+                list_text = strx.contents
+                # print(list_text)
+                vote_peroid = list_text[1].string
+                vote_time = list_text[3].string
+                vote_result = list_text[5].find('span').text
+                if vote_result != '':
+                    print('开奖期数:' + vote_peroid, '开奖时间：' + vote_time, '开奖结果：' + vote_result)
+            except Exception as e:
+                print('搜索数据错误，列表出错')
+        dealy_time = vote_retime + 30
+        print('延时%s刷新' % dealy_time)
+        time.sleep(dealy_time)
+
+
+class Thread(QThread):
+    def __init__(self):
+        super(Thread, self).__init__()
+
+    def run(self):
+        try:
+            do_16()
+        except Exception as e:
+            print(repr(e))
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     mw = Main_win()
     mw.show()
-    mw.por()
     sys.exit(app.exec_())
