71eb282b09d61412f4891a0ba1e84d2f9bcac849
Company_work <ssolsu1@126.com>
2018-02-09 17:33:15 +0800

    登录成功123

--- juxiangyou.py
+++ juxiangyou.py
@@ -123,15 +123,15 @@ def do_16():
         "Referer": "http://www.juxiangyou.com/",
         "User-Agent": "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64;Trident/5.0)"
     }
-
     while True:
         vote_retime = 0
         current_period = ''
         multiple = [0, 1, 2, 4, 6, 8, 16, 18, 20, 23, 26, 30, 34, 40]
+        multiple = [0, 1, 3, 7, 15, 31, 63, 127, 255, 23, 26, 30, 34, 40]
         url = 'http://www.juxiangyou.com/fun/play/speed16/index'
         req = requests.get(url, cookies=gol_cookies, headers=header)
         soup = BeautifulSoup(req.text, 'lxml')
-        tr_text = soup.find_all('tr', limit=10)
+        tr_text = soup.find_all('tr', limit=12)
         # 查询当前投注信息
         vote_info = soup.find('label', attrs={'class': 'J_jcEnd'})
         # 判断是否刚好在开奖
@@ -154,21 +154,34 @@ def do_16():
                 print('搜索资料出错，列表错误')
                 print('traceback.format_exc():%s' % traceback.format_exc())
         # print(tr_text)
-        for strx in tr_text[1:7]:
-            try:
-                list_text = strx.contents
-                # print(list_text)
-                last_peroid = list_text[1].string
-                last_time = list_text[3].string
-                last_result = list_text[5].find('span').text
-                if last_result != '':
-                    last_result = int(last_result)
-                    print('上期期数:', last_peroid, '开奖时间：', last_time, '开奖结果：', last_result)
-                else:
-                    last_result = 0
-            except Exception as e:
-                print('搜索数据错误，列表出错')
-                print('traceback.format_exc():%s' % traceback.format_exc())
+        # 手动查找上几期数据
+        if current_period != '':
+            s1 = str(int(current_period) - 1)
+            s2 = str(int(current_period) - 2)
+            s3 = str(int(current_period) - 3)
+            s4 = str(int(current_period) - 4)
+            last_1 = soup.find('td', text=s1).find_next_sibling('td', attrs={'class': 'num'}).find('span').string
+            last_result = int(last_1)
+            last_2 = soup.find('td', text=s2).find_next_sibling('td', attrs={'class': 'num'}).find('span').string
+            last_3 = soup.find('td', text=s3).find_next_sibling('td', attrs={'class': 'num'}).find('span').string
+            last_4 = soup.find('td', text=s4).find_next_sibling('td', attrs={'class': 'num'}).find('span').string
+            expend_list = list(set([last_2, last_3, last_4]))
+            print('上期期数:', s1, '开奖结果：', last_result, '多余列表', expend_list)
+        # for strx in tr_text[1:10]:
+        #     try:
+        #         list_text = strx.contents
+        #         print(list_text)
+        #         last_peroid = list_text[1].string
+        #         last_time = list_text[3].string
+        #         last_result = list_text[5].find('span').text
+        #         if last_result != '':
+        #             last_result = int(last_result)
+        #             print('上期期数:', last_peroid, '开奖时间：', last_time, '开奖结果：', last_result)
+        #         else:
+        #             last_result = 0
+        #     except Exception as e:
+        #         print('搜索数据错误，列表出错')
+        #         print('traceback.format_exc():%s' % traceback.format_exc())
         if vote_list:  # 如果不为空，说明上一次投注了，判断是否正确。
             if last_result in vote_list:
                 print('投注正确,倍率清空')
@@ -184,17 +197,15 @@ def do_16():
             # 这个部分还需要判断上一起是否为正确，如果不正确，倍数翻倍，如果正确，倍数归1---------
             pass
             # 复数
-            if int(current_period) - int(last_peroid) == 1:
-                print('当前期减上一期为1,数据没有错误')
             if last_result % 2 == 0:
                 # 投注中单
-                # print('上期结果是中双')
-                vote_list = vote_thing(current_period, last_result, 0, multiple[wrong])
+                print('上期结果是中双')
+                vote_list = vote_thing(current_period, last_result, 0, multiple[wrong], expend_list)
 
             elif last_result % 2 == 1:
                 # 投注中双
-                # print('上期结果是中单')
-                vote_list = vote_thing(current_period, last_result, 1, multiple[wrong])
+                print('上期结果是中单')
+                vote_list = vote_thing(current_period, last_result, 1, multiple[wrong], expend_list)
 
         else:
             vote_list = []
@@ -207,24 +218,36 @@ def do_16():
         time.sleep(dealy_time)
 
 
-def vote_thing(vote_current, last_result, sp_flag, multiple):  # 负责投注的函数
+def vote_thing(vote_current, last_result, sp_flag, multiple, expend_list):  # 负责投注的函数
     list_num = [1, 3, 6, 10, 15, 21, 25, 27, 27, 25, 21, 15, 10, 6, 3, 1]
     return_list = []
     if sp_flag == 0:  # 投注中单
-        list_num[6] = list_num[6] * 10 * multiple
-        list_num[8] = list_num[8] * 10 * multiple
-        list_num[10] = list_num[10] * 10 * multiple
-        list_num[last_result - 3] = list_num[last_result - 3] * 10 * multiple
+        list_num[6] = list_num[6] * 50 * multiple
+        list_num[8] = list_num[8] * 50 * multiple
+        list_num[10] = list_num[10] * 50 * multiple
+        list_num[last_result - 3] = list_num[last_result - 3] * 50 * multiple
         # print('投注买中单和上一期结果,倍数为:' + str(multiple))
         return_list = [9, 11, 13, last_result]
+        for x in expend_list:
+            print('x:', x)
+            if int(x) < 8 or int(x) > 13:
+                list_num[int(x) - 3] = list_num[int(x) - 3] * 50 * multiple
+                return_list.append(int(x))
+
+
     else:  # 投注中双
         # print('投注买中单和上一期结果,倍数为:' + str(multiple))
-        list_num[5] = list_num[5] * 10 * multiple
-        list_num[7] = list_num[7] * 10 * multiple
-        list_num[9] = list_num[9] * 10 * multiple
-        list_num[last_result - 3] = list_num[last_result - 3] * 10 * multiple
+        list_num[5] = list_num[5] * 50 * multiple
+        list_num[7] = list_num[7] * 50 * multiple
+        list_num[9] = list_num[9] * 50 * multiple
+        list_num[last_result - 3] = list_num[last_result - 3] * 50 * multiple
         return_list = [8, 10, 12, last_result]
-    print('经过修改后，投注的额度是列表是：', list_num, '倍率', multiple)
+        for x in expend_list:
+            print('x:', x)
+            if int(x) < 8 or int(x) > 13:
+                list_num[int(x) - 3] = list_num[int(x) - 3] * 50 * multiple
+                return_list.append(int(x))
+    print('经过修改后，投注的额度是列表是：', list_num, '倍率:', multiple)
     base_time = int(time.time()) * 1000
     x_sign = baseN(base_time, 36)
     post_head = {"Accept": "application/json, text/javascript, */*; q=0.01",
@@ -245,13 +268,18 @@ def vote_thing(vote_current, last_result, sp_flag, multiple):  # 负责投注的
     pst_data = {'jxy_parameter': a, 'timestamp': base_time}
     url = 'http://www.juxiangyou.com/fun/play/interaction'
     # Post数据服务器，cookies使用登录页面与验证码 合并cookies提交
-    req = requests.post(url, data=pst_data, cookies=gol_cookies, headers=post_head,
-                        allow_redirects=False, timeout=5)
-    # print('打印投注返回信息:', req.text)
-    vote_status = json.loads(req.text)['code']
-    if vote_status == '10000':
-        print(vote_current + '投注成功')
-    return return_list
+    try:
+        req = requests.post(url, data=pst_data, cookies=gol_cookies, headers=post_head,
+                            allow_redirects=False, timeout=10)
+        # print('打印投注返回信息:', req.text)
+        vote_status = json.loads(req.text)['code']
+        if vote_status == '10000':
+            print(vote_current + '投注成功')
+        print('购买的列表是', return_list)
+        return return_list
+    except Exception as e:
+        print('购买的列表是', return_list)
+        return []
 
 
 class Thread(QThread):
