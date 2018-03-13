db7de94333e7e59c42d6abe1fdaac31379e5720b
Company_work <ssolsu1@126.com>
2018-03-13 18:20:35 +0800

    登录成功111

--- A310.py
+++ A310.py
@@ -1,16 +1,8 @@
 # -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file '0310-1118.ui'
-#
-# Created by: PyQt5 UI code generator 5.9.1
-#
-# WARNING! All changes made in this file will be lost!
-
-from PyQt5 import QtCore, QtGui, QtWidgets
-import sqlite3, time
+from PyQt5 import QtCore, QtWidgets
+import time,sys
 from PyQt5.QtCore import QThread, pyqtSignal
 import pymssql
-import sys
 
 
 class Ui_MainWindow(object):
@@ -38,6 +30,7 @@ class Ui_MainWindow(object):
         self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
         self.lineEdit = QtWidgets.QLineEdit(self.layoutWidget)
         self.lineEdit.setObjectName("lineEdit")
+
         self.gridLayout.addWidget(self.lineEdit, 0, 1, 1, 1)
         self.pushButton = QtWidgets.QPushButton(self.layoutWidget)
         self.pushButton.setObjectName("pushButton")
@@ -50,7 +43,7 @@ class Ui_MainWindow(object):
         self.tableWidget = QtWidgets.QTableWidget(self.groupBox_2)
         self.tableWidget.setGeometry(QtCore.QRect(10, 20, 1031, 231))
         self.tableWidget.setObjectName("tableWidget")
-        self.tableWidget.setColumnCount(7)
+        self.tableWidget.setColumnCount(8)
         self.tableWidget.setRowCount(0)
         self.tableWidget.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
         self.groupBox_3 = QtWidgets.QGroupBox(self.centralwidget)
@@ -69,12 +62,12 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "聚声泰系统物料查询"))
         self.groupBox.setTitle(_translate("MainWindow", "查询窗口"))
         self.label.setText(_translate("MainWindow", "查询："))
         self.pushButton.setText(_translate("MainWindow", "查  询"))
         self.groupBox_2.setTitle(_translate("MainWindow", "剩余库存"))
-        self.groupBox_3.setTitle(_translate("MainWindow", "物料应用"))
+        self.groupBox_3.setTitle(_translate("MainWindow", "物料反查/派单查询"))
         column_name = [
             '料号',
             '名称',
@@ -82,69 +75,128 @@ class Ui_MainWindow(object):
             '物料属性',
             '单位',
             '仓库',
+            '批次',
             '数量',
         ]
         self.tableWidget.setHorizontalHeaderLabels(column_name)  # 设置列名称
+        self.tableWidget.setColumnWidth(0, 100)
+        self.tableWidget.setColumnWidth(1, 170)
+        self.tableWidget.setColumnWidth(2, 200)
+        self.tableWidget.setColumnWidth(3, 80)
+        self.tableWidget.setColumnWidth(4, 60)
+        self.tableWidget.setColumnWidth(5, 150)
+        self.tableWidget.setColumnWidth(6, 150)
+        self.tableWidget.setColumnWidth(6, 140)
+        self.tableWidget.verticalHeader().setVisible(False)
+        self.tableWidget.setSelectionBehavior(QtWidgets.QAbstractItemView.SelectRows)
         # 创建一条线程
 
     def update_item_data(self, data):
-        server = '192.168.1.97'
-        user = 'sa'
-        password = 'xfcctv1983'
-        result = []
-        try:
-            conn = pymssql.connect(server, user, password, "AIS20180226172529", charset='GBK')
-            cur = conn.cursor()
-            cur.execute(
-                "SELECT a.FNumber,a.FName,a.FModel,a.FErpClsID,b.FName FROM t_ICItem as a join t_MeasureUnit as b on a.FUnitID=b.FItemID  where a.FNumber='" + str(
-                    data) + "'")
-            result = cur.fetchall()
-            print(result)
-        except Exception as e:
-            print(repr(e))
-        finally:
-            conn.close()
-        conn.close()
-        if len(result[0]) > 4:
-            self.tableWidget.setColumnWidth(0,100)
-            self.tableWidget.setColumnWidth(1, 100)
-            self.tableWidget.setColumnWidth(2, 100)
-            self.tableWidget.setColumnWidth(3, 100)
-            self.tableWidget.setColumnWidth(4, 100)
-            self.tableWidget.setColumnWidth(5, 100)
-            self.tableWidget.setColumnWidth(6, 100)
-            self.tableWidget.setColumnWidth(7, 100)
-            dqhs = self.tableWidget.rowCount()
-            self.tableWidget.insertRow(dqhs)
-            self.tableWidget.setItem(dqhs, 0, QtWidgets.QTableWidgetItem(result[0][0]))
+        yrow=self.tableWidget.rowCount()
+        self.tableWidget.setRowCount(0)
+        self.tableWidget.clearContents()
+        length = len(data)
+        utf8_length = len(data.encode('utf-8'))
+        length = (utf8_length - length) / 2 + length
+        if length >= 3:
+            server = '192.168.1.97'
+            user = 'sa'
+            password = 'xfcctv1983'
+            result = []
+            result1 = []
+            yichun=[]
+            conn = ''
+            cur = ''
+            try:
+                conn = pymssql.connect(server, user, password, "AIS20180101102412", charset='GBK')
+                cur = conn.cursor()
+                cur.execute(
+                    "SELECT a.FNumber,a.FName,a.FModel,a.FErpClsID,b.FName,a.FItemID FROM t_ICItem as a join t_MeasureUnit as b on a.FUnitID=b.FItemID  where a.FNumber like '%" + str(
+                        data) + "%' or a.fmodel like '%" + str(data) + "%'")
+                result = cur.fetchall()
+                # cur.close()
+                print(result)
+                if result:
+                    for a in result:
+                        cur = conn.cursor()
+                        result1_sql = "SELECT a.FQty,b.fname,a.FBatchNo,a.fitemid FROM ICInventory as a join t_Stock as b on a.FStockID=b.fitemid where a.FQty<>0 AND a.FItemID='" + str(
+                            a[
+                                5]) + "' UNION ALL SELECT a.FQty,b.fname,a.FBatchNo,a.fitemid FROM POInventory as a join t_Stock as b on a.FStockID=b.fitemid where a.fqty>0 and a.FItemID='" + str(
+                            a[5]) + "'"
+                        cur.execute(result1_sql)
+                        result1 = cur.fetchall()
+                        print(result1)
+                        if result1:
+                            for b in result1:
+                                yichun.append(b[3])
+                                dqhs = self.tableWidget.rowCount()
+                                self.tableWidget.insertRow(dqhs)
+                                self.tableWidget.setItem(dqhs, 0, QtWidgets.QTableWidgetItem(str(a[0])))
+                                # 名称
+                                self.tableWidget.setItem(dqhs, 1, QtWidgets.QTableWidgetItem(str(a[1])))
+                                # 规格
+                                self.tableWidget.setItem(dqhs, 2, QtWidgets.QTableWidgetItem(str(a[2])))
+                                # 属性
+                                if a[3] == 2:
+                                    self.tableWidget.setItem(dqhs, 3, QtWidgets.QTableWidgetItem('自制'))
+                                if a[3] == 1:
+                                    self.tableWidget.setItem(dqhs, 3, QtWidgets.QTableWidgetItem('外购'))
+                                # 单位
+                                self.tableWidget.setItem(dqhs, 4, QtWidgets.QTableWidgetItem(str(a[4])))
+                                # 仓库
+                                self.tableWidget.setItem(dqhs, 5, QtWidgets.QTableWidgetItem(str(b[1])))
+                                # 批次
+                                self.tableWidget.setItem(dqhs, 6, QtWidgets.QTableWidgetItem(str(b[2])))
+                                # 数量
+                                self.tableWidget.setItem(dqhs, 7, QtWidgets.QTableWidgetItem(str(round(b[0], 2))))
+                        else:
+                            dqhs = self.tableWidget.rowCount()
+                            self.tableWidget.insertRow(dqhs)
+                            self.tableWidget.setItem(dqhs, 0, QtWidgets.QTableWidgetItem(str(a[0])))
+                            # 名称
+                            self.tableWidget.setItem(dqhs, 1, QtWidgets.QTableWidgetItem(str(a[1])))
+                            # 规格
+                            self.tableWidget.setItem(dqhs, 2, QtWidgets.QTableWidgetItem(str(a[2])))
+                            # 属性
+                            if a[3] == 2:
+                                self.tableWidget.setItem(dqhs, 3, QtWidgets.QTableWidgetItem('自制'))
+                            if a[3] == 1:
+                                self.tableWidget.setItem(dqhs, 3, QtWidgets.QTableWidgetItem('外购'))
+                            # 单位
+                            self.tableWidget.setItem(dqhs, 4, QtWidgets.QTableWidgetItem(str(a[4])))
 
-            self.tableWidget.setItem(dqhs, 1, QtWidgets.QTableWidgetItem(result[0][1]))
-            self.tableWidget.setItem(dqhs, 2, QtWidgets.QTableWidgetItem(result[0][2]))
-            if result[0][3] == 2:
-                self.tableWidget.setItem(dqhs, 3, QtWidgets.QTableWidgetItem('自制'))
-            if result[0][3] == 1:
-                self.tableWidget.setItem(dqhs, 3, QtWidgets.QTableWidgetItem('外购'))
-            self.tableWidget.setItem(dqhs, 4, QtWidgets.QTableWidgetItem(result[0][4]))
+                else:
+                    QtWidgets.QMessageBox.information(self.centralwidget, "提示", "没有任何你所查询物料的信息",
+                                                      QtWidgets.QMessageBox.Yes)
+                    self.lineEdit.setFocus()
+            except Exception as e:
+                print(repr(e))
+            finally:
+                if conn != '':
+                    conn.close()
+            if cur != '':
+                cur.close()
+                conn.close()
+        else:
+            QtWidgets.QMessageBox.information(self.centralwidget, "提示", "你查询的内容少于3个中文或6个字母符号",
+                                              QtWidgets.QMessageBox.Yes)
+            self.lineEdit.setFocus()
 
     def update_table_row(self, data):
         self.tableWidget.setRowCount(data)
 
     def chaxun_clicked(self):
         self.t1 = UpdateTable()
-        # bb = pyqtSignal(str)
-        # print('啊啊啊啊啊')
         chaxun_content = self.lineEdit.text()
-        # bb.emit(chaxun_content)
         self.t1.jieshou(chaxun_content)
         self.t1.update_date.connect(self.update_item_data)  # 链接信号
-        self.t1.update_1.connect(self.update_table_row)
         self.t1.start()
 
 
 class UpdateTable(QThread):
     """更新数据类"""
 
-    update_date = pyqtSignal(str)  # pyqt5 支持python3的str，没有Qstring
+    update_date = pyqtSignal(str)
     update_1 = pyqtSignal(int)
 
     def jieshou(self, data):
@@ -152,7 +204,13 @@ class UpdateTable(QThread):
 
     def run(self):
         print('线程中获取到的数据', self.cx)
-        # global chaxun_data
-        # print('run线程中运行的chaxun_data数据',chaxun_data)
         self.update_date.emit(self.cx)
         time.sleep(0.01)
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
