e5ac4bc34f3c47b52d26e52a198bccf9f4a693ed
Administrator <ssolsu1@126.com>
2018-03-14 00:48:59 +0800

    4321233556

--- juxiangyou/jxy_test.py
+++ juxiangyou/jxy_test.py
@@ -1,6 +1,7 @@
 from PyQt5.QtWidgets import QWidget, QTableWidget, QApplication, QPushButton, QVBoxLayout, QTableWidgetItem, \
     QAbstractItemView
 import sys, time
+from PyQt5.QtGui import QColor
 from threading import Thread
 from PyQt5.QtCore import QThread, pyqtSignal
 import sqlite3
@@ -30,12 +31,26 @@ class mywindow(QWidget):
             '小',
             '单',
             '双',
-            '买中状态',
-            '买中正确率',
+            '状态',
+            '连错',
             '买大小',
         ]
         self.talbe1.setHorizontalHeaderLabels(column_name)  # 设置列名称
         self.talbe1.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        self.talbe1.setColumnWidth(0,100)
+        self.talbe1.setColumnWidth(1, 100)
+        self.talbe1.setColumnWidth(2, 100)
+        self.talbe1.setColumnWidth(3, 100)
+        self.talbe1.setColumnWidth(4, 50)
+        self.talbe1.setColumnWidth(5, 50)
+        self.talbe1.setColumnWidth(6, 50)
+        self.talbe1.setColumnWidth(7, 50)
+        self.talbe1.setColumnWidth(8, 50)
+        self.talbe1.setColumnWidth(9, 50)
+        self.talbe1.setColumnWidth(10, 50)
+        self.talbe1.setColumnWidth(11, 50)
+        self.talbe1.setColumnWidth(12, 50)
+        self.talbe1.setColumnWidth(13, 50)
         vlayout.addWidget(self.yunsuan)
         vlayout.addWidget(self.talbe1)
         self.setLayout(vlayout)
@@ -70,137 +85,119 @@ class mywindow(QWidget):
     def update_js1(self):
         rowcout = self.talbe1.rowCount()
         flag = 0
-
-        for i in range(0, rowcout):
+        wrong=0
+        for i in range(0, rowcout - 20):
             maizhongflag = 0
-            dxjg=''
-            temp_list = []
-            dqjg = self.talbe1.item(i, 3).text()
-            xia1 = self.talbe1.item(i + 1, 3).text()
-            xia2 = self.talbe1.item(i + 2, 3).text()
-            xia3 = self.talbe1.item(i + 3, 3).text()
-            xia4 = self.talbe1.item(i + 4, 3).text()
-            xia5 = self.talbe1.item(i + 5, 3).text()
-            if int(xia1) > 7 and int(xia1) < 20:
+            dxjg = ''
+            vode_side = 0
+            vode_dx = -1
+            t_lst = []
+            dqjg = int(self.talbe1.item(i, 3).text())
+            xia1 = int(self.talbe1.item(i + 1, 3).text())
+            xia2 = int(self.talbe1.item(i + 2, 3).text())
+            xia3 = int(self.talbe1.item(i + 3, 3).text())
+            xia4 = int(self.talbe1.item(i + 4, 3).text())
+            xia5 = int(self.talbe1.item(i + 5, 3).text())
+            if xia1 > 7 and xia1 < 20:
+                vode_side = 1
                 for w in range(1, 20):
-                    temp_list.append(self.talbe1.item(i + w, 3).text())
-                # print('说明下一期是中:')
-                # 首先需要判断是不是中边中结构，如果是，搜索下面20期是否有中边中结构，如果有，假如是中，则购买，如果没有则跳过
-                if (int(xia2) < 8 or int(xia2) > 19) and (int(xia3) > 7 and int(xia3) < 20):
-                    print('满足当前是中边中结构')
-                    for x in range(i + 3, i + 3 + 10):
-                        if int(self.talbe1.item(x, 3).text()) > 7 and int(self.talbe1.item(x, 3).text()) < 20:
-                            sou1 = int(self.talbe1.item(x + 1, 3).text())
-                            sou2 = int(self.talbe1.item(x + 2, 3).text())
-                            print('找到一个中，开始搜索中边中结构', sou1, sou2)
-                            if (sou1 < 8 or sou1 > 19) and (sou2 > 7 and sou2 < 20):
-                                print('获取到一个中边中结构，查询是否需要')
-                                sou3 = int(self.talbe1.item(x - 1, 3).text())
-                                # print('sou3', sou3)
-                                if sou3 > 7 and sou3 < 20:
-                                    maizhongflag = 1
-                                    dxjg=maidaxiao(xia1,xia2,xia3,xia4,xia5,temp_list)
-                                    self.talbe1.setItem(i, 10, QTableWidgetItem('买中'+str(dxjg)))
-                                    print('这期要买中')
+                    t_lst.append(int(self.talbe1.item(i + w, 3).text()))
+                print(t_lst)
+                if (xia2 < 8 or xia2 > 19) and (xia3 > 7 and xia3 < 20):
+                    vode_side = 0
+                    for index, y in enumerate(t_lst[1:-10]):
+                        if y > 7 and y < 20:
+                            if (t_lst[index + 2] < 8 or t_lst[index + 2] > 19) and t_lst[index + 3] > 7 and t_lst[
+                                        index + 3] < 20:
+                                if t_lst[index] > 7 and t_lst[index] < 20:
+                                    vode_side = 1
+                                break
+                elif (xia2 > 7 and xia2 < 20) and (xia3 < 8 or xia3 > 19):
+                    for index, y in enumerate(t_lst[1:-10]):
+                        if y > 7 and y < 20:
+                            if (t_lst[index + 2] > 7 and t_lst[index + 2] < 20) and t_lst[index + 3] < 8 or t_lst[
+                                        index + 3] > 19:
+                                if t_lst[index] < 8 or t_lst[index] > 19:
+                                    vode_side = 0
+                                break
+                elif (xia2 > 7 and xia2 < 20) and (xia3 > 7 and xia3 < 20) and (xia4 < 8 or xia4 > 19):
+                    for index, y in enumerate(t_lst[1:-10]):
+                        if y > 7 and y < 20:
+                            if (t_lst[index + 2] > 7 and t_lst[index + 2] < 20) and (t_lst[index + 3] > 7 and t_lst[
+                                    index + 3] < 20) and (t_lst[index + 4] < 8 or t_lst[
+                                    index + 4] > 19):
+                                if t_lst[index] < 8 or t_lst[index] > 19:
+                                    vode_side = 0
+                                break
+                if xia1 < 14:
+                    index=0
+                    vode_dx = 0
+                    if xia2 > 13:
+                        for index, y in enumerate(t_lst[1:-3]):
+                            if y < 14:
+                                if (t_lst[index + 2] > 13):
+                                    print(y, index, t_lst[index + 2],t_lst[index])
+                                    if t_lst[index] > 13:
+                                        vode_dx = 1
+                                        print(self.talbe1.item(i,0).text(),t_lst,t_lst[index + 2],t_lst[index],index)
                                     break
-                                else:
-                                    print('这期要跳过')
-                                    self.talbe1.setItem(i, 10, QTableWidgetItem('跳过'))
+                    elif xia2 < 14 and xia3 > 13:
+                        for index, y in enumerate(t_lst[1:-3]):
+                            if y < 14:
+                                if (t_lst[index + 2] < 14 and t_lst[index + 3] > 13):
+                                    if t_lst[index] > 13:
+                                        vode_dx = 1
                                     break
-                            else:
-                                print('这期要跳过')
-                                self.talbe1.setItem(i, 10, QTableWidgetItem('跳过'))
+                    # elif xia2 < 14 and xia3 < 14 and xia4 > 13:
+                    #     for index, y in enumerate(t_lst[1:-4]):
+                    #         if y < 14:
+                    #             if (t_lst[index + 2] < 14 and t_lst[index + 3] < 14 and t_lst[index + 4] > 13):
+                    #                 if t_lst[index] > 13:
+                    #                     vode_dx = 1
+                    #                     break
                 else:
-                    # 说明下一期是中，但不是中边中结构,第一，判断是否2连中，3连中，4连中结构
-                    # 2连中结构
-                    if int(xia2) > 7 and int(xia2) < 20 and (int(xia3) < 8 or int(xia3) > 19):
-                        print('发现一个中中结构')
-                        for x in range(i + 3, i + 3 + 10):
-                            if int(self.talbe1.item(x, 3).text()) > 7 and int(self.talbe1.item(x, 3).text()) < 20:
-                                sou1 = int(self.talbe1.item(x + 1, 3).text())
-                                sou2 = int(self.talbe1.item(x + 2, 3).text())
-                                print('找到一个中，开始搜索中中结构', sou1, sou2)
-                                if sou1 > 7 and sou1 < 20:
-                                    print('满足中中结构')
-                                    if sou2 < 8 or sou2 > 19:
-                                        self.talbe1.setItem(i, 10, QTableWidgetItem('中中跳过'))
-                                    else:
-                                        maizhongflag = 1
-                                        dxjg = maidaxiao(xia1, xia2, xia3, xia4, xia5, temp_list)
-                                        self.talbe1.setItem(i, 10, QTableWidgetItem('中中买中'+str(dxjg)))
-
-                                        if int(dqjg) > 7 and int(dqjg) < 20:
-                                            self.talbe1.setItem(i, 11, QTableWidgetItem('正确'))
-                                        else:
-                                            self.talbe1.setItem(i, 11, QTableWidgetItem('错误'))
+                    index=0
+                    vode_dx = 1
+                    if xia2 < 14:
+                        for index, y in enumerate(t_lst[1:-3]):
+                            if y > 13:
+                                if (t_lst[index + 2] < 14):
+                                    if t_lst[index] < 14:
+                                        vode_dx = 0
                                     break
-                                else:
-                                    maizhongflag = 1
-                                    dxjg = maidaxiao(xia1, xia2, xia3, xia4, xia5, temp_list)
-                                    self.talbe1.setItem(i, 10, QTableWidgetItem('中中买中'+str(dxjg)))
-                    # 3连结构
-                    elif int(xia2) > 7 and int(xia2) < 20 and (int(xia3) > 7 and int(xia3) < 20) and (
-                                    int(xia4) < 8 or int(xia4) > 19):
-                        print('发现一个中中中结构')
-                        for x in range(i + 3, i + 3 + 15):
-                            if int(self.talbe1.item(x, 3).text()) > 7 and int(self.talbe1.item(x, 3).text()) < 20:
-                                sou1 = int(self.talbe1.item(x + 1, 3).text())
-                                sou2 = int(self.talbe1.item(x + 2, 3).text())
-                                sou3 = int(self.talbe1.item(x + 3, 3).text())
-                                print('找到一个中，开始搜索中中结构', sou1, sou2, sou3)
-                                if sou1 > 7 and sou1 < 20 and sou2 > 7 and sou2 < 20:
-                                    print('满足中中中结构')
-                                    if sou3 < 8 or sou3 > 19:
-                                        self.talbe1.setItem(i, 10, QTableWidgetItem('中中中跳过'))
-                                    else:
-                                        maizhongflag = 1
-                                        dxjg = maidaxiao(xia1, xia2, xia3, xia4, xia5, temp_list)
-                                        self.talbe1.setItem(i, 10, QTableWidgetItem('中中中买中'+str(dxjg)))
-                                        if int(dqjg) > 7 and int(dqjg) < 20:
-                                            self.talbe1.setItem(i, 11, QTableWidgetItem('正确'))
-                                        else:
-                                            self.talbe1.setItem(i, 11, QTableWidgetItem('错误'))
+                    elif xia2 > 13 and xia3 < 14:
+                        for index, y in enumerate(t_lst[1:-3]):
+                            if y > 13:
+                                if (t_lst[index + 2] > 13 and t_lst[index + 3] < 14):
+                                    if t_lst[index] < 14:
+                                        vode_dx = 0
                                     break
-                                else:
-                                    maizhongflag = 1
-                                    dxjg = maidaxiao(xia1, xia2, xia3, xia4, xia5, temp_list)
-                                    self.talbe1.setItem(i, 10, QTableWidgetItem('中中中买中'+str(dxjg)))
-                    # 4连中结构
-                    elif int(xia2) > 7 and int(xia2) < 20 and (int(xia3) > 7 and int(xia3) < 20) and (
-                                    int(xia4) > 7 and int(xia4) < 20) and (int(xia5) < 8 or int(xia5) > 19):
-                        print('发现一个中中中中结构')
-                        for x in range(i + 3, i + 3 + 15):
-                            if int(self.talbe1.item(x, 3).text()) > 7 and int(self.talbe1.item(x, 3).text()) < 20:
-                                sou1 = int(self.talbe1.item(x + 1, 3).text())
-                                sou2 = int(self.talbe1.item(x + 2, 3).text())
-                                sou3 = int(self.talbe1.item(x + 3, 3).text())
-                                sou4 = int(self.talbe1.item(x + 4, 3).text())
-                                print('找到一个中，开始搜索中中中中结构', sou1, sou2, sou3)
-                                if sou1 > 7 and sou1 < 20 and sou2 > 7 and sou2 < 20 and sou3 > 7 and sou3 < 20:
-                                    print('满足中中中中结构')
-                                    if sou4 < 8 or sou4 > 19:
-                                        self.talbe1.setItem(i, 10, QTableWidgetItem('中中中中跳过'))
-                                    else:
-                                        maizhongflag = 1
-                                        dxjg = maidaxiao(xia1, xia2, xia3, xia4, xia5, temp_list)
-                                        self.talbe1.setItem(i, 10, QTableWidgetItem('中中中中买中'+str(dxjg)))
-                                        if int(dqjg) > 7 and int(dqjg) < 20:
-                                            self.talbe1.setItem(i, 11, QTableWidgetItem('正确'))
-                                        else:
-                                            self.talbe1.setItem(i, 11, QTableWidgetItem('错误'))
-                                    break
-                                else:
-                                    maizhongflag = 1
-                                    dxjg = maidaxiao(xia1, xia2, xia3, xia4, xia5, temp_list)
-                                    self.talbe1.setItem(i, 10, QTableWidgetItem('中中中中买中'+str(dxjg)))
+                    # elif xia2 > 13 and xia3 > 13 and xia4 < 14:
+                    #     for index, y in enumerate(t_lst[1:-4]):
+                    #         if y >13:
+                    #             if (t_lst[index + 2] > 13 and t_lst[index + 3] > 13 and t_lst[index + 4] < 14):
+                    #                 if t_lst[index] < 14:
+                    #                     vode_dx = 0
+                    #                     break
+                if vode_side == 1 and vode_dx==0:
+                    if (dqjg<14 and dqjg>7):
+                        wrong=0
+                        self.talbe1.setItem(i, 10, QTableWidgetItem('买小正确'))
+                        self.talbe1.setItem(i,11,QTableWidgetItem(str(wrong)))
                     else:
-                        dxjg = maidaxiao(xia1, xia2, xia3, xia4, xia5, temp_list)
-                        self.talbe1.setItem(i, 10, QTableWidgetItem('直接买中'+str(dxjg)))
-                        maizhongflag = 1
-                        if int(dqjg) > 7 and int(dqjg) < 20:
-                            self.talbe1.setItem(i, 11, QTableWidgetItem('正确'))
-                        else:
-                            self.talbe1.setItem(i, 11, QTableWidgetItem('错误'))
+                        wrong=wrong+1
+                        self.talbe1.setItem(i, 10, QTableWidgetItem('买小错误'))
+                        self.talbe1.setItem(i, 11, QTableWidgetItem(str(wrong)))
+
+                elif vode_side==1 and vode_dx==1:
+                    if (dqjg>13 and dqjg <20):
+                        wrong=0
+                        self.talbe1.setItem(i,10,QTableWidgetItem('买大正确'))
+                        self.talbe1.setItem(i, 11, QTableWidgetItem(str(wrong)))
+                    else:
+                        wrong=wrong+1
+                        self.talbe1.setItem(i, 10, QTableWidgetItem('买大错误'))
+                        self.talbe1.setItem(i, 11, QTableWidgetItem(str(wrong)))
 
 
 def maidaxiao(xia1, xia2, xia3, xia4, xia5, temp_list):
@@ -210,7 +207,6 @@ def maidaxiao(xia1, xia2, xia3, xia4, xia5, temp_list):
     xia3 = int(xia3)
     xia4 = int(xia4)
     xia5 = int(xia5)
-    print(temp_list)
     # 下一期是大
     if xia1 > 13:
         # 判断是不是大小大，如果是
@@ -224,10 +220,10 @@ def maidaxiao(xia1, xia2, xia3, xia4, xia5, temp_list):
                         # 找到了一个大小大，这时候，需要看看列表中上一期是什么
                         if int(temp_list[index + 1]) > 13:
                             maida = 1
-                        break
+                break
         else:
-            #独大模式要关注
-            if xia2<14:
+            # 独大模式要关注
+            if xia2 < 14:
                 for index, x in enumerate(temp_list[2:-5]):
                     # 找到了一个大
                     if int(x) > 13:
@@ -235,7 +231,7 @@ def maidaxiao(xia1, xia2, xia3, xia4, xia5, temp_list):
                         if int(temp_list[index + 3]) > 13:
                             print('独大模式，直接买大')
                             maida = 1
-                            break
+                    break
             # 双大形态判断开始
             elif xia2 > 13 and xia3 < 14:
                 # 找到独立双大
@@ -247,7 +243,7 @@ def maidaxiao(xia1, xia2, xia3, xia4, xia5, temp_list):
                             if int(temp_list[index + 4]) > 13:
                                 print('连续大，直接买大')
                                 maida = 1
-                            break
+                    break
             # 查找三连大
             elif xia2 > 13 and xia3 > 13 and xia4 < 14:
                 # 找到独立三大
@@ -273,7 +269,7 @@ def maidaxiao(xia1, xia2, xia3, xia4, xia5, temp_list):
                                 maida = 1
                             break
             else:
-                maida=1
+                maida = 1
         return maida
 
 
@@ -284,7 +280,7 @@ class thead(QThread):
         conn = sqlite3.connect('shuju.db')
         cur = conn.cursor()
         # self.update_1.emit(rowcount[0])
-        sql = "select * from jxy_fk28  where vote_time like '%03-10%' OR vote_time LIKE '%03-09%' ORDER by period DESC limit 500"
+        sql = "select * from jxy_fk28  where vote_time like '%03-11%' OR vote_time LIKE '%03-10%' ORDER by period DESC limit 1500"
         cur.execute(sql)
         result = cur.fetchall()
         for index, i in enumerate(result):
