292a2c9d903c16b222ead9bf22e038c5d546492e
Company_work <ssolsu1@126.com>
2018-04-04 17:33:35 +0800

    登录成功111

--- A310.py
+++ A310.py
@@ -3,6 +3,12 @@ from PyQt5 import QtCore, QtWidgets
 import time,sys
 from PyQt5.QtCore import QThread, pyqtSignal
 import pymssql
+import uuid
+import _mssql
+import decimal
+print(decimal.__version__)
+print(uuid.ctypes.__version__)
+print(_mssql.__version__)
 
 
 class Ui_MainWindow(object):
@@ -19,7 +25,7 @@ class Ui_MainWindow(object):
         self.groupBox.setCheckable(False)
         self.groupBox.setObjectName("groupBox")
         self.layoutWidget = QtWidgets.QWidget(self.groupBox)
-        self.layoutWidget.setGeometry(QtCore.QRect(10, 20, 258, 43))
+        self.layoutWidget.setGeometry(QtCore.QRect(10, 20, 910, 43))
         self.layoutWidget.setObjectName("layoutWidget")
         self.gridLayout = QtWidgets.QGridLayout(self.layoutWidget)
         self.gridLayout.setContentsMargins(10, 10, 10, 10)
@@ -27,7 +33,10 @@ class Ui_MainWindow(object):
         self.gridLayout.setObjectName("gridLayout")
         self.label = QtWidgets.QLabel(self.layoutWidget)
         self.label.setObjectName("label")
+        self.label_1 = QtWidgets.QLabel(self.layoutWidget)
+        self.label.setObjectName("label_1")
         self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
+        self.gridLayout.addWidget(self.label_1, 0, 3, 1, 1)
         self.lineEdit = QtWidgets.QLineEdit(self.layoutWidget)
         self.lineEdit.setObjectName("lineEdit")
 
@@ -65,6 +74,7 @@ class Ui_MainWindow(object):
         MainWindow.setWindowTitle(_translate("MainWindow", "聚声泰系统物料查询"))
         self.groupBox.setTitle(_translate("MainWindow", "查询窗口"))
         self.label.setText(_translate("MainWindow", "查询："))
+        self.label_1.setText(_translate("MainWindow", "(可以模糊查询料号或规格型号,如3.01.1001 或 Φ9.2)"))
         self.pushButton.setText(_translate("MainWindow", "查  询"))
         self.groupBox_2.setTitle(_translate("MainWindow", "剩余库存"))
         self.groupBox_3.setTitle(_translate("MainWindow", "物料反查/派单查询"))
@@ -98,7 +108,7 @@ class Ui_MainWindow(object):
         length = len(data)
         utf8_length = len(data.encode('utf-8'))
         length = (utf8_length - length) / 2 + length
-        if length >= 3:
+        if length >= 2:
             server = '192.168.1.97'
             user = 'sa'
             password = 'xfcctv1983'
@@ -108,62 +118,61 @@ class Ui_MainWindow(object):
             conn = ''
             cur = ''
             try:
-                conn = pymssql.connect(server, user, password, "AIS20180101102412", charset='GBK')
+                conn = pymssql.connect(server, user, password, "AIS20180101102412")
                 cur = conn.cursor()
-                cur.execute(
-                    "SELECT a.FNumber,a.FName,a.FModel,a.FErpClsID,b.FName,a.FItemID FROM t_ICItem as a join t_MeasureUnit as b on a.FUnitID=b.FItemID  where a.FNumber like '%" + str(
-                        data) + "%' or a.fmodel like '%" + str(data) + "%'")
+                sql="SELECT a.FNumber,a.FName,a.FModel,a.FErpClsID,b.FName,a.FItemID FROM t_ICItem as a join t_MeasureUnit as b on a.FUnitID=b.FItemID  where a.FNumber like '%" + str(
+                        data) + "%' or a.fmodel like '%" + str(data) + "%'"
+                print(sql)
+                cur.execute(sql)
                 result = cur.fetchall()
                 # cur.close()
-                print(result)
+                # print(result)
                 if result:
                     for a in result:
                         cur = conn.cursor()
                         result1_sql = "SELECT a.FQty,b.fname,a.FBatchNo,a.fitemid FROM ICInventory as a join t_Stock as b on a.FStockID=b.fitemid where a.FQty<>0 AND a.FItemID='" + str(
-                            a[
-                                5]) + "' UNION ALL SELECT a.FQty,b.fname,a.FBatchNo,a.fitemid FROM POInventory as a join t_Stock as b on a.FStockID=b.fitemid where a.fqty>0 and a.FItemID='" + str(
+                            a[5]) + "' UNION ALL SELECT a.FQty,b.fname,a.FBatchNo,a.fitemid FROM POInventory as a join t_Stock as b on a.FStockID=b.fitemid where a.fqty>0 and a.FItemID='" + str(
                             a[5]) + "'"
                         cur.execute(result1_sql)
                         result1 = cur.fetchall()
                         print(result1)
                         if result1:
                             for b in result1:
-                                yichun.append(b[3])
                                 dqhs = self.tableWidget.rowCount()
                                 self.tableWidget.insertRow(dqhs)
-                                self.tableWidget.setItem(dqhs, 0, QtWidgets.QTableWidgetItem(str(a[0])))
+                                self.tableWidget.setItem(dqhs, 0, QtWidgets.QTableWidgetItem(str(a[0]).encode('latin1').decode('GBK')))
                                 # 名称
-                                self.tableWidget.setItem(dqhs, 1, QtWidgets.QTableWidgetItem(str(a[1])))
+                                self.tableWidget.setItem(dqhs, 1, QtWidgets.QTableWidgetItem(str(a[1]).encode('latin1').decode('GBK')))
                                 # 规格
-                                self.tableWidget.setItem(dqhs, 2, QtWidgets.QTableWidgetItem(str(a[2])))
+                                self.tableWidget.setItem(dqhs, 2, QtWidgets.QTableWidgetItem(str(a[2]).encode('latin1').decode('GBK')))
                                 # 属性
                                 if a[3] == 2:
                                     self.tableWidget.setItem(dqhs, 3, QtWidgets.QTableWidgetItem('自制'))
                                 if a[3] == 1:
                                     self.tableWidget.setItem(dqhs, 3, QtWidgets.QTableWidgetItem('外购'))
                                 # 单位
-                                self.tableWidget.setItem(dqhs, 4, QtWidgets.QTableWidgetItem(str(a[4])))
+                                self.tableWidget.setItem(dqhs, 4, QtWidgets.QTableWidgetItem(str(a[4]).encode('latin1').decode('GBK')))
                                 # 仓库
-                                self.tableWidget.setItem(dqhs, 5, QtWidgets.QTableWidgetItem(str(b[1])))
+                                self.tableWidget.setItem(dqhs, 5, QtWidgets.QTableWidgetItem(str(b[1]).encode('latin1').decode('GBK')))
                                 # 批次
-                                self.tableWidget.setItem(dqhs, 6, QtWidgets.QTableWidgetItem(str(b[2])))
+                                self.tableWidget.setItem(dqhs, 6, QtWidgets.QTableWidgetItem(str(b[2]).encode('latin1').decode('GBK')))
                                 # 数量
-                                self.tableWidget.setItem(dqhs, 7, QtWidgets.QTableWidgetItem(str(round(b[0], 2))))
+                                self.tableWidget.setItem(dqhs, 7, QtWidgets.QTableWidgetItem(str(round(b[0], 2)).encode('latin1').decode('GBK')))
                         else:
                             dqhs = self.tableWidget.rowCount()
                             self.tableWidget.insertRow(dqhs)
-                            self.tableWidget.setItem(dqhs, 0, QtWidgets.QTableWidgetItem(str(a[0])))
+                            self.tableWidget.setItem(dqhs, 0, QtWidgets.QTableWidgetItem(str(a[0]).encode('latin1').decode('GBK')))
                             # 名称
-                            self.tableWidget.setItem(dqhs, 1, QtWidgets.QTableWidgetItem(str(a[1])))
+                            self.tableWidget.setItem(dqhs, 1, QtWidgets.QTableWidgetItem(str(a[1]).encode('latin1').decode('GBK')))
                             # 规格
-                            self.tableWidget.setItem(dqhs, 2, QtWidgets.QTableWidgetItem(str(a[2])))
+                            self.tableWidget.setItem(dqhs, 2, QtWidgets.QTableWidgetItem(str(a[2]).encode('latin1').decode('GBK')))
                             # 属性
                             if a[3] == 2:
                                 self.tableWidget.setItem(dqhs, 3, QtWidgets.QTableWidgetItem('自制'))
                             if a[3] == 1:
                                 self.tableWidget.setItem(dqhs, 3, QtWidgets.QTableWidgetItem('外购'))
                             # 单位
-                            self.tableWidget.setItem(dqhs, 4, QtWidgets.QTableWidgetItem(str(a[4])))
+                            self.tableWidget.setItem(dqhs, 4, QtWidgets.QTableWidgetItem(str(a[4]).encode('latin1').decode('GBK')))
 
                 else:
                     QtWidgets.QMessageBox.information(self.centralwidget, "提示", "没有任何你所查询物料的信息",
@@ -176,7 +185,7 @@ class Ui_MainWindow(object):
                     conn.close()
             if cur != '':
                 cur.close()
-                conn.close()
+            conn.close()
         else:
             QtWidgets.QMessageBox.information(self.centralwidget, "提示", "你查询的内容少于3个中文或6个字母符号",
                                               QtWidgets.QMessageBox.Yes)
